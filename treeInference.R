f66a3a74d487c603c2904cbab91642cf5828d8d3
cromanpa94 <cromanpa94@gmail.com>
2025-02-17 17:41:35 -0700

    Efficient tree building

--- treeInference.R
+++ treeInference.R
@@ -2,9 +2,12 @@ library(ape)
 library(phytools)
 library(combinat)
 library(phangorn)
+#packageurl <- "https://cran.r-project.org/src/contrib/Archive/smacof/smacof_1.10-8.tar.gz"
+#install.packages(packageurl, repos=NULL, type="source")
+library(smacof) #1.10-8
 
 #Tree building
-size = 20
+size = 100
 replicates = 100
 aln_prefix = "alignment"
 aln_model = "GTR"
@@ -33,42 +36,67 @@ system(paste0("iqtree2 --alisim ", aln_prefix, " -m ", aln_model,
 ))
 
 ## Partition the alignment to run on 4 species each time
-dist_trees <- function(alns = alns, sfraction = 0.5, export = TRUE){
+generate_fixed_element_combinations <- function(vector, fixed_element, group_size, num_combinations) {
+  # Ensure fixed_element is in the vector
+  if (!(fixed_element %in% vector)) {
+    stop("The specified fixed element is not in the given vector.")
+  }
 
+  # Remove the fixed element from the available choices
+  remaining_elements <- setdiff(vector, fixed_element)
+
+  # Check if there are enough elements to form valid groups
+  if (length(remaining_elements) < (group_size - 1)) {
+    stop("Not enough elements to form the required group size.")
+  }
+
+  # Generate num_combinations unique random groups
+  combinations <- replicate(num_combinations,
+                            c(fixed_element, sample(remaining_elements, group_size - 1, replace = FALSE)),
+                            simplify = FALSE)
+
+  # Convert to a matrix for easy viewing
+  combination_matrix <- data.frame(do.call(cbind, combinations))
+
+  return(combination_matrix)
+}
+
+# For each alignment
+# For each target species (100 in this case), find 10 unique quartets
+# 20k quartets per species per alignment
+
+dist_trees <- function(alns = alns, group_size = 4, num_combinations = 10, export = TRUE){
   trees_p_aln <- pblapply(alns, function(y){
-
     data <- as.phyDat(read.dna(y)) # full dataset
-    testComb <- combn(names(data), 4) # find all combinations (4 terminals)
-    ## Create trees for each alignment (MDS etc)
-    subsetCols <- sample(1:ncol(testComb), round(sfraction*ncol(testComb)) )
-
-    trees_4 <- lapply(subsetCols, function(x){
-      tryCatch({
-        subdata <- data[testComb[,x]]
+    t_taxa <- names(data)
+    t_all_t_taxa <-  pblapply(t_taxa, function(z){
+      subsetCols <- generate_fixed_element_combinations(vector = t_taxa, fixed_element = z, group_size = group_size, num_combinations = num_combinations)
+      trees_ttaxa <- lapply(subsetCols, function(x){
+        tryCatch({
+        subdata <- data[x,]
         dm <- dist.ml(subdata)
         dm[is.na(dm)] <- 0
 
         fit_reg <- mds(dm, ndim = 2)
         mds_tree <- as.phylo(hclust(dm))
         mds_tree
-      }, error=function(e){})
+        }, error=function(e){})
+      })
+      trees_ttaxa[sapply(trees_ttaxa, is.null)] <- NULL
+      trees_ttaxa
     })
-
-    trees_4[sapply(trees_4, is.null)] <- NULL
-    trees_4
-    class(trees_4) <- "multiPhylo"
+    fullTrees <- do.call(c, unlist(t_all_t_taxa, recursive = FALSE))
     if(export == TRUE){
-    write.tree(trees_4, paste0(y, "tree_dist.tre"))
+      write.tree(fullTrees, paste0(y, "tree_dist.tre"))
     }
-    trees_4
+    fullTrees
   })
-
   return(trees_p_aln)
 }
 
 ## Reconstruct phylogeny from tree distribution
 alns <- list.files(pattern = "alignment_")
-trees_d <- dist_trees(alns = alns[1:10], sfraction = 1, export = TRUE)
+trees_d <- dist_trees(alns = alns[1:10], group_size = 4, num_combinations = 10)
 
 treesR <- list.files(pattern = ".phytree_dist.tre")
 
@@ -80,10 +108,34 @@ lapply(treesR, function(y){
 
 treesRA <- list.files(pattern = ".out.tre")
 recTrees <- lapply(treesRA, read.tree)
+class(recTrees) <- "multiPhylo"
+
 tree <- read.tree("tree.nwk")
 treeDist <- sapply(recTrees, function(x){
-dist.topo(unroot(tree), unroot(x))/((length(which(tree$edge[,2] > Ntip(tree))) *2)) #Based on the number of internal branches
+  dist.topo(unroot(tree), unroot(x))/((length(which(tree$edge[,2] > Ntip(tree))) *2)) #Based on the number of internal branches
 })
 
 
 
+require(ape)
+require(dplyr)
+library(ggtree)
+
+tree_ <- rmtree(1, 10)
+trees <- rmtree(5, 10)
+time.trees <- lapply(1:length(trees), function(i) {
+  tree <- trees[[i]]
+  tree$tip.label <- paste0("t", 1:10)
+  dates <- estimate.dates(tree, 1:10, mu=1, nsteps=1)
+  tree$edge.length <- dates[tree$edge[, 2]] - dates[tree$edge[, 1]]
+  fortify(tree) %>% mutate(tree=factor(i, levels=as.character(1:10)))
+})
+
+# Plot multiple trees with aligned tips from muliple time points
+btrees <- read.tree(system.file("extdata/RAxML",
+                                "RAxML_bootstrap.H3",
+                                package="treeio")
+)[1:10]
+
+ggdensitree(btrees, alpha=.3, colour='steelblue') +
+  geom_tiplab(size=3) + hexpand(.35)
